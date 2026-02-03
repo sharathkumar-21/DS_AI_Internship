540d0be5f5207ddb1fbf6768fb31c005a7512d3b
S R SHARATH KUMAR <Sharath7506@gmail.com>
2026-02-03 14:46:09 +0530

    first commit

---
+++ src/day02/Day2.py
@@ -0,0 +1,46 @@
+# # # num1=int(input("Enter num1: "))
+# # # num2=int(input("Enter num2: "))
+# # # operation = input("Enter operation (+, -, *, /, **, %): ")
+# # # if operation == "+":
+# # #     result = num1 + num2
+# # # elif operation == "-":
+# # #     result = num1 - num2
+# # # elif operation == "*":
+# # #     result = num1 * num2
+# # # elif operation == "/":
+# # #     result = num1 / num2
+# # # elif operation == "**":
+# # #     result = num1 ** num2
+# # # elif operation == "%":
+# # #     result = num1 % num2
+# # # print("Result:", result)
+
+
+# # name="sharath"
+# # msg="welcome"
+# # print(f"hi {name},{msg}everyone")
+
+#  task-1
+print("TASK 1")
+name = input("Enter your name: ")
+age = float(input("enter your age: "))
+age = int(age)
+new_age =4 + age
+print(f"hey,,,{name} , you will be {new_age} , years old in 2030") 
+
+#task-2
+print("TASK 2")
+Amount = float(input("Enter the Total Bill Amount : "))
+People =int(input("Enter the number of the peoples : "))
+share=Amount/People
+print(f'Total Bill : {Amount}. Each person pays: {share:.2f}')
+
+#task-3
+print("TASK 3")
+item_name="Mobile"
+quantity=4
+price=1999.98
+in_stock=True
+print("item:",item_name,", Qty:",quantity,", Price:",price,", Available:",in_stock)
+total_cost=quantity*price
+print("Total Cost: ",total_cost)
---
+++ src/day03/day03.py
@@ -0,0 +1,8 @@
+# numbers = [10,20,30,40]
+# coordinates =(5,10)
+# print(numbers)
+# print(coordinates)
+
+A=[100,200,300,400,500,600,700,800,]
+A.sort(reverse=True)
+print(A)
