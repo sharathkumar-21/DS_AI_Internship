4b750d6f439d423c4000dfa7ddda5dceac09c664
S R SHARATH KUMAR <Sharath7506@gmail.com>
2026-02-05 11:21:38 +0530

    first commit

--- src/day03/day03.py
+++ src/day03/day03.py
@@ -1,38 +1,38 @@
-# numbers = [10,20,30,40]
-# coordinates =(5,10)
-# print(numbers)
-# print(coordinates)
+numbers = [10,20,30,40]
+coordinates =(5,10)
+print(numbers)
+print(coordinates)
 
-# A=[100,200,300,400,500,600,700,800,]
-# A.sort(reverse=True)
-# print(A)
+A=[100,200,300,400,500,600,700,800,]
+A.sort(reverse=True)
+print(A)
 
-# print("task-1"(inventory_manager))
-# inventory_containing = ["apples","dates","bananas","carrots",]
-# print(inventory_containing)
+print("task-1"(inventory_manager))
+inventory_containing = ["apples","dates","bananas","carrots",]
+print(inventory_containing)
 
-# inventory_containing.append("eggs")
-# print(inventory_containing)
+inventory_containing.append("eggs")
+print(inventory_containing)
 
-# inventory_containing.remove("bananas")
-# print(inventory_containing)
+inventory_containing.remove("bananas")
+print(inventory_containing)
 
-# inventory_containing.sort()
-# print(inventory_containing)
+inventory_containing.sort()
+print(inventory_containing)
 
-# print("task-2"(the data slicing))
-# temperatures = [22, 24, 25, 28, 30, 29, 27, 26, 24, 22]
-# print(temperatures[0])
-# print(temperatures[-1])
-# print(temperatures[3:6])
-# print(temperatures[-3:])
+print("task-2"(the data slicing))
+temperatures = [22, 24, 25, 28, 30, 29, 27, 26, 24, 22]
+print(temperatures[0])
+print(temperatures[-1])
+print(temperatures[3:6])
+print(temperatures[-3:])
 
 print("task(3-immutable config)")
 
 screen_res =(1920,1080)
 print(screen_res)
 
-# screen_res[0]=(1280)
-# print(screen_res)
+screen_res[0]=(1280)
+print(screen_res)
 
 print("TUPLES CANNOT BE MODIFIED")
